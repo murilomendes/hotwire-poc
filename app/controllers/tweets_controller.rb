feffc49d6b95fd3666999e7242752f8d741947eb
M.M <mmendes090@live.com>
2021-10-13 22:40:32 -0300

    Add tags

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,69 +1,76 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: %i[ show edit update destroy ]
-
-  # GET /tweets or /tweets.json
+  before_action :set_tweet, only: %i[show edit update destroy]
+ 
   def index
-    @tweets = Tweet.all
-  end
-
-  # GET /tweets/1 or /tweets/1.json
-  def show
-  end
-
-  # GET /tweets/new
-  def new
+    @tweets = Tweet.all.order(created_at: :desc)
     @tweet = Tweet.new
   end
-
-  # GET /tweets/1/edit
-  def edit
-  end
-
-  # POST /tweets or /tweets.json
+ 
   def create
     @tweet = Tweet.new(tweet_params)
-
+ 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: "Tweet was successfully created." }
+        format.html do
+          redirect_to tweets_path
+        end
         format.json { render :show, status: :created, location: @tweet }
       else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            @tweet, partial: 'tweets/form', locals: { tweet: @tweet }
+          )
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
     end
   end
-
-  # PATCH/PUT /tweets/1 or /tweets/1.json
+ 
+  def edit; end
+ 
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: "Tweet was successfully updated." }
+    if params[:like] == 'true'
+      @tweet.increment!(:likes)
+      respond_to do |format|
+        format.html { redirect_to @tweet }
         format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    else
+      respond_to do |format|
+        if @tweet.update(tweet_params)
+          format.html { redirect_to @tweet }
+          format.json { render :show, status: :ok, location: @tweet }
+        else
+          format.turbo_stream do
+            render turbo_stream: turbo_stream.replace(
+              @tweet, partial: 'tweets/form', locals: { tweet: @tweet }
+            )
+          end
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @tweet.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
-
-  # DELETE /tweets/1 or /tweets/1.json
+ 
+  def show; end
+ 
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.html { redirect_to tweets_url }
       format.json { head :no_content }
     end
   end
-
+ 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tweet
-      @tweet = Tweet.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def tweet_params
-      params.require(:tweet).permit(:body, :likes)
-    end
+ 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+ 
+  def tweet_params
+    params.require(:tweet).permit(:body, :likes)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-​
+
 Rails.start()
 ActiveStorage.start()
-​
+
 import "controllers"
